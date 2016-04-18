52af864dc76abe6925d38520c9a197ad17133f1d
huangziyi01 <huangziyi01@baidu.com>
2016-04-18 09:15:33 +0800

    quick push

--- demo/lists/label.js
+++ demo/lists/label.js
@@ -5,7 +5,7 @@ export default class Demo extends React.Component {
     render() {
         return (
             <Switch label="我喜欢Fit"
-                    labelWidth="100"/>
+                    labelWidth={100}/>
         )
     }
 }
